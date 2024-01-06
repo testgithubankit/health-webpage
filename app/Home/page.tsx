502f1ece3a46ad4f88d6cdf9afca74796c91870c
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2024-01-06 13:01:39 +0530

    updated

---
+++ app/Home/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// import SearchSlider from './SearchSlider';
+// import AboutPlatform from './AboutPlatform';
+// import HouseProvider from './HouseProvider';
+// import CaseReview from './CaseReview';
+// import CareJourney from './CareJourney';
+
+const Home = () => {
+  return (
+    <>
+    <h1>Hi i am Home</h1>
+    {/* <SearchSlider />
+    <HouseProvider />
+    <AboutPlatform />
+    <CaseReview />
+    <CareJourney /> */}
+    </>
+   
+  )
+}
+
+export default Home
